cc4d0a048ca109c257f59707c1f5dd1e15ee13a6
Dawoozies <davidchriswiggins@gmail.com>
2024-04-02 12:04:35 +1100

    Made pixel size smaller.

--- Assets/ImageEffectCamera.cs
+++ Assets/ImageEffectCamera.cs
@@ -8,15 +8,30 @@ using Object = UnityEngine.Object;
 public class ImageEffectCamera : MonoBehaviour
 {
     private Camera mainCamera;
+    public RenderTexture imageMaterialEffect;
+    private Vector2Int currentDimensions;
     void Start()
     {
         mainCamera = Camera.main;
         SceneManager.activeSceneChanged += ChangedActiveScene;
         DontDestroyOnLoad(gameObject);
+        currentDimensions = new Vector2Int(mainCamera.pixelWidth, mainCamera.pixelHeight);
+        imageMaterialEffect.width = currentDimensions.x;
+        imageMaterialEffect.height = currentDimensions.y;
     }
 
     private void Update()
     {
+        if (currentDimensions.x != mainCamera.pixelWidth || currentDimensions.y != mainCamera.pixelHeight)
+        {
+            currentDimensions.x = mainCamera.pixelWidth;
+            currentDimensions.y = mainCamera.pixelHeight;
+            imageMaterialEffect.Release();
+            imageMaterialEffect.width = currentDimensions.x;
+            imageMaterialEffect.height = currentDimensions.y;
+            imageMaterialEffect.Create();
+            mainCamera.targetTexture = imageMaterialEffect;
+        }
         transform.position = mainCamera.transform.position;
     }
     private void ChangedActiveScene(Scene current, Scene next)
